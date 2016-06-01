5f5c8c97c0fa03b585a2cb922086f735e3aadb95
williamwhitejr <wwhitejr.sh@mysummitps.org>
2016-06-01 20:57:29 +0000

    update

---
+++ info.js
@@ -0,0 +1,28 @@
+var item1 = document.getElementById("item1");
+var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
+var info1 = document.getElementById("info1");
+var info2 = document.getElementById("info2");
+var info3 = document.getElementById("info3");
+
+item1.addEventListener("click", function(){
+    info1.innerHTML = "Sports are very common among many people. For years and years there have been many sports that people would watch and play. One sport that is very popular in America is American football. The very first game of football was on November 6, 1869. Rugby was the football type game that peple played back then. After a game this rugby player from Yale changed the rules of rugby that transformed into football. ";
+
+});
+
+
+item2.addEventListener("click", function(){
+    info1.innerHTML = "Sports are very common among many people. For years and years there have been many sports that people would watch and play. One sport that is very popular in America is American football. The very first game of football was on November 6, 1869. Rugby was the football type game that peple played back then. After a game this rugby player from Yale changed the rules of rugby that transformed into football. ";
+  
+});
+
+item3.addEventListener("click", function(){
+    info1.innerHTML = "Basketball is a very fun game played by many people many ages. Basketball is very popular in China, Argentina and France. Basketball originated in 1891 when a minister named James Naismith was told to teach P.E to a class of young men. This was in Springfield, Massachusetts.";
+  
+});
+
+item4.addEventListener("click", function(){
+    info1.innerHTML = " Soccer is a very fun and popular sport world wide. Some places soccer are popular in are South Africa, Sweden, and Sudan.Soccer was recored to be back than 2000 years ago. It was thought that the history of soccer came from ancient China. Others say soccer came from England.";
+
+});   
---
+++ model.js
@@ -0,0 +1,11 @@
+var b2 = document.getElementById("b2")
+var b1 = document.getElementById("b1")
+var image = document.getElementById("image")
+var head = document.getElementById("head")
+var step = 1;
+
+b2.addEventListener("click", function(){
+        step++;
+        if (step)
+})
+ 
